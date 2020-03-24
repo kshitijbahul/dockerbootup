f4841fd24b9585b5c937f727dbc1f25669e1cd4c
KshitijBahul <chunks.3@gmail.com>
2020-03-24 17:20:06 +0530

    Testing Restart policies

--- multi-container/index.js
+++ multi-container/index.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const redis = require("redis")
+const process = require("process")
 
 const app= express();
 //Connecting to the redis server
@@ -10,6 +11,7 @@ const client=redis.createClient({
 client.set('visits',0);
 
 app.get("/",(req,res)=>{
+    process.exit(0)
     client.get('visits',(err,visits)=>{
         res.send(`No of vists is ${visits}`)
         client.set('visits',parseInt(visits)+1)
